f59bf8deccd4def99f822bb5cccc001bb6428348
Eppapassanini <79142052941SashJ@gmail.com>
2026-03-04 00:45:38 +1000

    Delete app directory

--- app/src/main/java/com/example/tvmazeshows/TvMazeApp.kt
+++
@@ -1,8 +0,0 @@
-
-package com.example.tvmazeshows
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class TvMazeApp : Application()
--- app/src/main/java/com/example/tvmazeshows/ui/state/UiState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.tvmazeshows.ui.state
-
-sealed class UiState<out T> {
-    object Loading : UiState<Nothing>()
-    data class Content<T>(val data: T) : UiState<T>()
-    data class Error(val message: String, val retryAction: (() -> Unit)? = null) : UiState<Nothing>()
-}
