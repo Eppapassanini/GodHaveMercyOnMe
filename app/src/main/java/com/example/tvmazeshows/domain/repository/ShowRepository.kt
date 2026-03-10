d84af06dd37552a654921beb11c2cf803698e657
Eppapassanini <79142052941SashJ@gmail.com>
2026-03-10 18:15:31 +1000

    Update ShowRepository.kt
    Убрал поиск

--- app/src/main/java/com/example/tvmazeshows/domain/repository/ShowRepository.kt
+++ app/src/main/java/com/example/tvmazeshows/domain/repository/ShowRepository.kt
@@ -5,5 +5,4 @@ import com.example.tvmazeshows.domain.model.Show
 interface ShowRepository {
     suspend fun getShows(page: Int = 0): Result<List<Show>>
     suspend fun getShowDetails(showId: Int): Result<Show>
-    suspend fun searchShows(query: String): Result<List<Show>>
 }
