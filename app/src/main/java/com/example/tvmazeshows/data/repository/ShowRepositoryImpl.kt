ad2de28f46158a204720cf210d37c416c4f470f9
Eppapassanini <79142052941SashJ@gmail.com>
2026-03-10 18:13:27 +1000

    Update ShowRepositoryImpl.kt
    Убрал поиск

--- app/src/main/java/com/example/tvmazeshows/data/repository/ShowRepositoryImpl.kt
+++ app/src/main/java/com/example/tvmazeshows/data/repository/ShowRepositoryImpl.kt
@@ -1,40 +1,25 @@
 
-package com.example.tvmazeshows.data.repository
+package com.example.tvmazeshows.di
 
 import com.example.tvmazeshows.data.network.TvMazeApi
 import com.example.tvmazeshows.data.network.IoDispatcher
-import com.example.tvmazeshows.domain.model.Show
-import com.example.tvmazeshows.domain.model.toDomain
+import com.example.tvmazeshows.data.repository.ShowRepositoryImpl
 import com.example.tvmazeshows.domain.repository.ShowRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.withContext
-import javax.inject.Inject
 import javax.inject.Singleton
 
-@Singleton
-class ShowRepositoryImpl @Inject constructor(
-    private val api: TvMazeApi,
-    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
-) : ShowRepository {
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
 
-    override suspend fun getShows(page: Int): Result<List<Show>> =
-        withContext(ioDispatcher) {
-            runCatching {
-                api.getShows(page).map { it.toDomain() }
-            }
-        }
-
-    override suspend fun getShowDetails(showId: Int): Result<Show> =
-        withContext(ioDispatcher) {
-            runCatching {
-                api.getShowById(showId).toDomain()
-            }
-        }
-
-    override suspend fun searchShows(query: String): Result<List<Show>> =
-        withContext(ioDispatcher) {
-            runCatching {
-                api.searchShows(query).map { it.show.toDomain() }
-            }
-        }
+    @Provides
+    @Singleton
+    fun provideShowRepository(
+        api: TvMazeApi,
+        @IoDispatcher ioDispatcher: CoroutineDispatcher
+    ): ShowRepository = ShowRepositoryImpl(api, ioDispatcher)
 }
